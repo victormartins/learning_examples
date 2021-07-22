8dd87b6f098de106cb851f6b2e5a241dcb5631d7
Victor Martins <victormartins@azafinance.com>
2021-07-22 14:00:20 +0100

    added public_send

--- benchmarking/eval_vs_send.rb
+++ benchmarking/eval_vs_send.rb
@@ -1,3 +1,14 @@
+# Calculating -------------------------------------
+#                 eval     36.867k (± 5.7%) i/s -    186.440k in   5.075067s
+#                 send    736.645k (± 7.4%) i/s -      3.692M in   5.044326s
+#          public_send    728.383k (± 4.9%) i/s -      3.687M in   5.075179s
+
+# Comparison:
+#                 send:   736645.4 i/s
+#          public_send:   728382.7 i/s - same-ish: difference falls within error
+#                 eval:    36866.7 i/s - 19.98x  slower
+
+
 require 'benchmark/ips'
 require 'pry'
 
@@ -34,6 +45,16 @@ Benchmark.ips do |x|
 
       entity
     end
+
+    def with_public_send
+      entity = {}
+
+      ATTRIBUTES_TO_SET.each do |attr|
+        entity[attr] = public_send(attr)
+      end
+
+      entity
+    end
   end
 
   x.report('eval') do
@@ -41,6 +62,10 @@ Benchmark.ips do |x|
   end
 
   x.report('send') do
+    Entity.new.with_send
+  end
+
+  x.report('public_send') do
     Entity.new.with_send
   end
 
